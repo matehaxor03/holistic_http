431d73906631ffd8a2b193511e6d81761366f09f
matehaxor03 <matehaxor03>
2022-12-19 17:00:47 +1100

    take error out of result and add to main error array if any

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/matehaxor03/holistic_http
 
 go 1.19
 
-require github.com/matehaxor03/holistic_json v0.0.6
+require github.com/matehaxor03/holistic_json v0.0.8
 
-require github.com/matehaxor03/holistic_common v0.0.5 // indirect
+require github.com/matehaxor03/holistic_common v0.0.10
--- http_extension/Helper.go
+++ http_extension/Helper.go
@@ -14,6 +14,11 @@ func Nop() {
 
 func WriteResponse(w http.ResponseWriter, result json.Map, write_response_errors []error) {
 	keys := result.Keys()
+
+	if common.IsNil(write_response_errors) {
+		var temp_errors []error
+		write_response_errors = temp_errors
+	}
 	
 	if len(keys) != 1 {
 		write_response_errors = append(write_response_errors, fmt.Errorf(fmt.Sprintf("number of root keys is incorrect %s",keys)))
@@ -31,8 +36,16 @@ func WriteResponse(w http.ResponseWriter, result json.Map, write_response_errors
 		} else {
 			inner_map_found = true
 			inner_map_value = inner_map
+
+			inner_map_errors, inner_map_errors_errors := inner_map_value.GetErrors("[errors]")
+			if inner_map_errors_errors != nil {
+				write_response_errors = append(write_response_errors, inner_map_errors_errors...)
+			} else if inner_map_errors != nil {
+				write_response_errors = append(write_response_errors, inner_map_errors...)
+			}
 		}
 	}
+
 
 	if len(write_response_errors) > 0 {
 		if inner_map_found {
@@ -59,6 +72,6 @@ func WriteResponse(w http.ResponseWriter, result json.Map, write_response_errors
 	if result_as_string_errors == nil {
 		w.Write([]byte(json_payload_builder.String()))
 	} else {
-		w.Write([]byte(fmt.Sprintf("{\"unknown\":{\"[errors]\":\"%s\", \"data\":null}}", strings.ReplaceAll(fmt.Sprintf("%s", result_as_string_errors), "\"", "\\\""))))
+		w.Write([]byte(fmt.Sprintf("{\"unknown\":{\"[errors]\":\"%s\", \"data\":null}}", strings.ReplaceAll(fmt.Sprintf("%s", write_response_errors), "\"", "\\\""))))
 	}
 }
